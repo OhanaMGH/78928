880bf5e5285c5ab669a706e317d0fd67d15eb5b8
Ohana Monserrath Gutierrez Hernandez <zS22016080@estudiantes.uv.mx>
2025-03-07 10:38:02 -0600

    rutas en clase

--- flask01/app.py
+++ flask01/app.py
@@ -2,9 +2,24 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def hola_mundo():
     return "Hola mundo"
+
+@app.route('/despedir')
+def despedir():
+    return "adios!"
+
+@app.route('/hola')
+def hola():
+    return '<h1 style="color:red;"">Hola!<h1>'
+
+@app.route('/json')
+def algo():
+    return '{"nombre":"John"}'
+
+@app.route('/xml')
+def xml():
+    return '<?xml version="1,0"?><nombre>John</nombre>'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',
